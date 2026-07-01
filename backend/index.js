17f4088f91dff7a92719a46cd1214805f6129584
Pratham-k-dev <pratham.k3007@gmail.com>
2026-07-01 16:00:32 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   
   cors: {
-    origin: [process.env.CLIENT_ORIGIN || "http://localhost:5173"],
+    origin: ["*"],
     methods: ["GET", "POST"]
   }
 });
