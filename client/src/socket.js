09b73f78239afe097c5642e2fa7a82ac74bbc95d
Pratham-k-dev <pratham.k3007@gmail.com>
2026-07-01 16:11:52 +0530

    bugs

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,4 @@
 import { io } from "socket.io-client";
 
 const SERVER = import.meta.env.VITE_SERVER_URL || "http://localhost:4000";
-console.log("client: "+ import.meta.env.VITE_SERVER_URL)
 export const socket = io(SERVER, { autoConnect: false });
