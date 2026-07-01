c7215816432ae94d0add6963d6f6859172cc064f
Pratham-k-dev <pratham.k3007@gmail.com>
2026-07-01 14:47:04 +0530

    bugfix

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
-
+import dotenv from "dotenv";
 const SERVER = import.meta.env.VITE_SERVER_URL || "http://localhost:4000";
+console.log(import.meta.env.VITE_SERVER_URL);
 export const socket = io(SERVER, { autoConnect: false });
